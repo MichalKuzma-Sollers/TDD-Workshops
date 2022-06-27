037b9281cb8143e0ce653f306b79ee35499eabbe
MichalKuzma <michal.a.kuzma@gmail.com>
2022-06-27 18:06:45 +0200

    Add project files.

---
+++ TDD Workshops/DecisionLevels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TDD_Workshops
+{
+    public enum DecisionLevel
+    {
+        Ecc,
+        FourEyes,
+        SixEyes,
+        IbcFig
+    }
+
+    public class DecisionLevels
+    {
+        public DecisionLevel? GetDecisionLevel(double? proposedLimit, double? approvedLimit, string proposedRating)
+        {
+            return null;
+        }
+    }
+}
---
+++ TDD WorkshopsTests/DecisionLevelsTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD_Workshops;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Workshops.Tests
+{
+    [TestClass()]
+    public class DecisionLevelsTests
+    {
+        [TestMethod()]
+        public void GetDecisionLevelTest()
+        {
+            Assert.Fail();
+        }
+    }
+}

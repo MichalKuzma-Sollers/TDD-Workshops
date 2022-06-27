6f4c07835acf84f9e7653fcce0307a1deb909716
MichalKuzma <michal.a.kuzma@gmail.com>
2022-06-27 18:27:36 +0200

    Add an interface for thresholds checker

--- TDD Workshops/DecisionLevels.cs
+++ TDD Workshops/DecisionLevels.cs
@@ -12,6 +12,12 @@ namespace TDD_Workshops
 
     public class DecisionLevels
     {
+        private IThresholdsChecker thresholdsChecker;
+        public DecisionLevels(IThresholdsChecker thresholdsChecker)
+        {
+            this.thresholdsChecker = thresholdsChecker;
+        }
+
         public DecisionLevel? GetDecisionLevel(double? proposedLimit, double? approvedLimit, string proposedRating)
         {
             return null;

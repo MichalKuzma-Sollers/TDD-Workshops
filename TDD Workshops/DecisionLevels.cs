1d8b7da916825350362fa84ba535f9fd3ddb03fc
MichalKuzma <michal.a.kuzma@gmail.com>
2022-06-28 20:02:40 +0200

    Implement DecisionLevels to pass all tests

--- TDD Workshops/DecisionLevels.cs
+++ TDD Workshops/DecisionLevels.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TDD_Workshops
 {
@@ -12,6 +13,11 @@ namespace TDD_Workshops
 
     public class DecisionLevels
     {
+        private IList<String> ratingScale = new List<String>
+        {
+            "1-", "1", "1+", "2-", "2", "2+", "3-", "3", "3+", "4-", "4", "4+", "5-", "5", "5+", "6-", "6", "6+", "7-", "7", "7+", "8-", "8", "8+", "9-", "9", "9+", "10-", "10", "10+"
+        };
+
         private IThresholdsChecker thresholdsChecker;
         public DecisionLevels(IThresholdsChecker thresholdsChecker)
         {
@@ -20,6 +26,43 @@ namespace TDD_Workshops
 
         public DecisionLevel? GetDecisionLevel(double? proposedLimit, double? approvedLimit, string proposedRating)
         {
+            if (approvedLimit == null || approvedLimit == 0.0)
+                return DecisionLevel.SixEyes;
+            if (proposedLimit == null)
+                throw new WarningMessageException();
+
+            double limitIncrease = (double)((proposedLimit - approvedLimit) / approvedLimit);
+
+            int proposedRatingIndex = ratingScale.IndexOf(proposedRating);
+            if (limitIncrease > 0.0 && limitIncrease <= 0.1
+                && (proposedRatingIndex < ratingScale.IndexOf("4-") || proposedRatingIndex > ratingScale.IndexOf("7+"))) 
+            {
+                return thresholdsChecker.CheckThresholds(new HashSet<DecisionLevel> { DecisionLevel.Ecc, DecisionLevel.FourEyes, DecisionLevel.IbcFig, DecisionLevel.SixEyes });
+            }
+
+            if (limitIncrease <= 0.0 &&
+                (proposedRatingIndex < ratingScale.IndexOf("3-") || proposedRatingIndex > ratingScale.IndexOf("7+")))
+            {
+                return thresholdsChecker.CheckThresholds(new HashSet<DecisionLevel> { DecisionLevel.Ecc, DecisionLevel.FourEyes, DecisionLevel.IbcFig, DecisionLevel.SixEyes });
+            }
+
+            if (limitIncrease > 0.0 && limitIncrease <= 0.1
+                && (proposedRatingIndex >= ratingScale.IndexOf("4-") && proposedRatingIndex <= ratingScale.IndexOf("7+")))
+            {
+                return thresholdsChecker.CheckThresholds(new HashSet<DecisionLevel> { DecisionLevel.FourEyes, DecisionLevel.SixEyes });
+            }
+
+            if (limitIncrease <= 0.0 &&
+                (proposedRatingIndex >= ratingScale.IndexOf("3-") && proposedRatingIndex <= ratingScale.IndexOf("7+")))
+            {
+                return thresholdsChecker.CheckThresholds(new HashSet<DecisionLevel> { DecisionLevel.FourEyes, DecisionLevel.SixEyes });
+            }
+
+            if (limitIncrease > 0.1)
+            {
+                return DecisionLevel.FourEyes;
+            }
+
             return null;
         }
     }
